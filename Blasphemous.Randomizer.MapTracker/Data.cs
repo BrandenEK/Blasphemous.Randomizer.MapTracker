d1ddb7452b214b5f4202262ce48ad0d1ba17a4ca
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-09-25 14:54:45 -0400

    core: use file scoped everywhere

--- Blasphemous.Randomizer.MapTracker/Data.cs
+++ Blasphemous.Randomizer.MapTracker/Data.cs
@@ -1,27 +1,26 @@
 ﻿
-namespace Blasphemous.Randomizer.MapTracker
+namespace Blasphemous.Randomizer.MapTracker;
+
+internal static class Data
 {
-    internal static class Data
+    public static readonly string[] redCandleIds =
     {
-        public static readonly string[] redCandleIds =
-        {
-            "RB18", "RB19",
-        };
+        "RB18", "RB19",
+    };
 
-        public static readonly string[] blueCandleIds =
-        {
-            "RB25", "RB26",
-        };
+    public static readonly string[] blueCandleIds =
+    {
+        "RB25", "RB26",
+    };
 
-        public static readonly string[] amanecidaIds =
-        {
-            "QI107", "QI108", "QI109", "QI110", "PR101",
-        };
+    public static readonly string[] amanecidaIds =
+    {
+        "QI107", "QI108", "QI109", "QI110", "PR101",
+    };
 
-        public static readonly string[] guiltArenaIds =
-        {
-            "QI32", "QI33", "QI34", "QI35", "QI79", "QI80", "QI81",
-            "Arena_NailManager[1000]", "HE10", "Arena_NailManager[3000]", "RB34", "Arena_NailManager[5000]", "RB35", "RB36",
-        };
-    }
+    public static readonly string[] guiltArenaIds =
+    {
+        "QI32", "QI33", "QI34", "QI35", "QI79", "QI80", "QI81",
+        "Arena_NailManager[1000]", "HE10", "Arena_NailManager[3000]", "RB34", "Arena_NailManager[5000]", "RB35", "RB36",
+    };
 }
